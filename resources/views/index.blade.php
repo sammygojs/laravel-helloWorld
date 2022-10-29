722d667e5c4cf332f41b052ed15543f7a6e132fc
Sumit <akoliyasumit@hotmail.com>
2022-10-29 23:11:18 +0530

    layout with btsp grid

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,36 @@
 
 @section('content')
 <h2>Our content</h2>
+
+<div class="container-lg" style="margin: 0 auto;">
+    <div class="row mt-5">
+        <div class="col-log-4 col-md-4 col-sm-12 text-center mb-3 ">
+            <div class="card" style="width: 18rem;">
+            <img/>
+                <div class="card-body">
+                    <div class="card-title">title</div>
+                    <div class="card-text">content</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-log-4 col-md-4 col-sm-12 text-center mb-3 ">
+            <div class="card" style="width: 18rem;">
+            <img/>
+                <div class="card-body">
+                    <div class="card-title">title</div>
+                    <div class="card-text">content</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-log-4 col-md-4 col-sm-12 text-center mb-3 ">
+            <div class="card" style="width: 18rem;">
+            <img/>
+                <div class="card-body">
+                    <div class="card-title">title</div>
+                    <div class="card-text">content</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,37 @@
-<h1>Header</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- <h1 class="text-center">Header</h1> -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
